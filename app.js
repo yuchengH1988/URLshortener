436ad75916299a1e0f4de5124b1bd04fd4318920
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-03-06 11:35:10 +0800

    feat: add index&results html, bootstrap

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const PORT = 3000
+const app = express()
 
 // const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.get('/', (req, res) =>
+  res.render('index')
+)
+
+app.get('/results', (req, res) =>
+  res.render('results')
+)
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
