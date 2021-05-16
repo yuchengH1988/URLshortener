fc5dbcbd29c65067dbce7cd7e1a19cd16176aa53
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-05-16 21:56:58 +0800

    ...

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const PORT = 3000
 const app = express()
+const bodyParser = require('body-parser')
 
 // const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) =>
   res.render('index')
